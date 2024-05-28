01e257cad77c659f3762a4d984649521311c4723
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-05-28 15:02:19 +0200

    01: Basics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{http::StatusCode, response::IntoResponse, routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    server().await;
+}
+
+pub async fn server() {
+    let app: Router = Router::new().route("/api/test", get(test));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn test() -> impl IntoResponse {
+    println!("test API");
+
+    (StatusCode::ACCEPTED, "Hi there")
 }
