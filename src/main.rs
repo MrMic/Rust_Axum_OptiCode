7956fbd0d8f125b1cf8866f292dbf3930da028de
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-05-31 15:12:30 +0200

    Set up Sea-ORM & Migration

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 use axum::{http::StatusCode, response::IntoResponse, routing::get, Router};
+use chrono::Utc;
+use sea_orm::{Database, DatabaseConnection, EntityTrait, InsertResult, Set};
+use uuid::Uuid;
+
+use entity::user;
 
 #[tokio::main]
 async fn main() {
@@ -6,8 +11,12 @@ async fn main() {
 }
 
 pub async fn server() {
-    let app: Router = Router::new().route("/api/test", get(test));
+    // * NOTE: ROUTER  _________________________________________________________________
+    let app: Router = Router::new()
+        .route("/api/test", get(test))
+        .route("/api/user/insert", get(create_user));
 
+    // * NOTE: SERVER _________________________________________________________________
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app.into_make_service())
@@ -19,4 +28,24 @@ async fn test() -> impl IntoResponse {
     println!("test API");
 
     (StatusCode::ACCEPTED, "Hi there")
+}
+
+async fn create_user() -> impl IntoResponse {
+    // * NOTE: ════════════════════════════ DB CONNECTION ════════════════════════════
+    let db: DatabaseConnection =
+        Database::connect("postgresql://postgres:pepere@localhost:5432/BlogDB")
+            .await
+            .unwrap();
+
+    let user_model = user::ActiveModel {
+        name: Set("John".to_owned()),
+        email: Set("johndoe@me.com".to_owned()),
+        password: Set("secret".to_owned()),
+        uuid: Set(Uuid::new_v4()),
+        created_at: Set(Utc::now().naive_utc()),
+        ..Default::default()
+    };
+    let usr = user::Entity::insert(user_model).exec(&db).await.unwrap();
+
+    (StatusCode::ACCEPTED, "User created").into_response()
 }
