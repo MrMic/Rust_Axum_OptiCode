00845e88382b2bb26a41c54233692133fd431737
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-06-06 15:01:03 +0200

    🍺 feat: add Create

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,18 @@ use axum::{
     http::StatusCode,
     response::IntoResponse,
     routing::{get, post},
-    Router,
+    Json, Router,
 };
 use chrono::Utc;
-use sea_orm::{Database, DatabaseConnection, EntityTrait, Set};
+use sea_orm::ActiveModelTrait;
+use sea_orm::{Database, DatabaseConnection, Set};
 use uuid::Uuid;
 
 use entity::user;
+
+use crate::models::user_models::CreateUserModel;
+
+mod models;
 
 #[tokio::main]
 async fn main() {
@@ -16,12 +21,12 @@ async fn main() {
 }
 
 pub async fn server() {
-    // * NOTE: ROUTER  _________________________________________________________________
+    // * INFO: ROUTER  _________________________________________________________________
     let app: Router = Router::new()
         .route("/api/test", get(test))
-        .route("/api/user/insert", get(create_user));
+        .route("/api/user/insert", post(create_user_post));
 
-    // * NOTE: SERVER _________________________________________________________________
+    // * INFO: SERVER _________________________________________________________________
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app.into_make_service())
@@ -35,22 +40,23 @@ async fn test() -> impl IntoResponse {
     (StatusCode::ACCEPTED, "Hi there")
 }
 
-async fn create_user() -> impl IntoResponse {
-    // * NOTE: ════════════════════════════ DB CONNECTION ════════════════════════════
+async fn create_user_post(Json(user_data): Json<CreateUserModel>) -> impl IntoResponse {
+    // * INFO: ════════════════════════════ DB CONNECTION ════════════════════════════
     let db: DatabaseConnection =
-        Database::connect("postgresql://postgres:pepere@172.23.0.3:5432/BlogDB")
+        Database::connect("postgresql://postgres:pepere@172.23.0.2:5432/BlogDB")
             .await
             .unwrap();
 
     let user_model = user::ActiveModel {
-        name: Set("John".to_owned()),
-        email: Set("johndoe@me.com".to_owned()),
-        password: Set("secret".to_owned()),
+        name: Set(user_data.name.to_owned()),
+        email: Set(user_data.email.to_owned()),
+        password: Set(user_data.password.to_owned()),
         uuid: Set(Uuid::new_v4()),
         created_at: Set(Utc::now().naive_utc()),
         ..Default::default()
     };
-    let _usr = user::Entity::insert(user_model).exec(&db).await.unwrap();
+    user_model.insert(&db).await.unwrap();
 
+    db.close().await.unwrap();
     (StatusCode::ACCEPTED, "User created").into_response()
 }
